cba635ed69ca5d9db2e66c1104bb0d3e776c58f7
Canan Kilic <canan03kilic@gmail.com>
2023-01-26 18:50:14 +0300

    porjedeki youm satırları düzenlendi

--- credit_limit_calculator.c
+++ credit_limit_calculator.c
@@ -1,16 +1,4 @@
-/* (Credit-Limit Calculator) Develop a C program that will determine whether a
-department-store customer has exceeded the credit limit on a charge account. For
-each customer, the following facts are available:
-a) Account number
-b) Balance at the beginning of the month
-c) Total of all items charged by this customer this month
-d) Total of all credits applied to this customer's account this month
-e) Allowed credit limit
-The program should use scanf to input each fact, calculate the new balance (=
-beginning balance + charges – credits), and determine whether the new balance exceeds
-the customer's credit limit. For those customers whose credit limit is exceeded, the
-program should display the customer's account number, credit limit, new balance
-and the message “Credit limit exceeded.” */
+
 
 #include <stdio.h>
 #include <stdbool.h>
